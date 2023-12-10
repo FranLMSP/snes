072c17648cc5522932c212bd6422c0f94fb60521
Franco Colmenarez <info@francoacg.com>
2023-12-10 16:28:34 -0500

    sample egui app

--- snes-frontend/src/main.rs
+++ snes-frontend/src/main.rs
@@ -1,5 +1,23 @@
-use snes_core::emulator::Emulator;
+use eframe::egui;
 
-fn main() {
-    let mut emulator = Emulator::new();
+fn main() -> eframe::Result<()> {
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native("SNES Emulator", native_options, Box::new(|cc| Box::new(SnesEmulatorApp::new(cc))))
+}
+
+#[derive(Default)]
+struct SnesEmulatorApp {}
+
+impl SnesEmulatorApp {
+    fn new(_cc: &eframe::CreationContext<'_>) -> Self {
+        Self::default()
+    }
+}
+
+impl eframe::App for SnesEmulatorApp {
+   fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+       egui::CentralPanel::default().show(ctx, |ui| {
+           ui.heading("Hello World!");
+       });
+   }
 }
